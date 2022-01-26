b41666fadbee456d43df1e5851a791560f6091e3
ankitt01 <ankitrawat0102@gmail.com>
2022-01-27 00:49:51 +0530

    changed folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import Weather from './components/Weather'
+// import Weather from './components/Weather'
+import './index.css'
 function App() {
+  const api = {
+    key: "f2db5a456819ebf40cda3295573c317a",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
   return (
-    <div className="App">
-      <Weather />
+    <div>
+        <main className="app">
+            <div className="search-box">
+                <input type="text" className="search-bar" placeholder='Search..'/>
+            </div>
+        </main>
     </div>
   );
 }
